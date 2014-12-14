c0a7261b54690b55c505b55100c090d097bdfff4
Aldo Stracquadanio <aldo.strac@gmail.com>
2014-12-14 19:40:56 +0000

    Re-write using Monoid, Semigroup and friends

--- src/main/scala/astrac/jsonz/Jsonz.scala
+++ src/main/scala/astrac/jsonz/Jsonz.scala
@@ -3,54 +3,55 @@ package astrac.jsonz
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import scala.reflect.ClassTag
+import scala.util.matching.Regex
 import scalaz._
 import Scalaz._
 import scala.collection.immutable.Iterable
 
 case class ValidationError(error: String, path: List[String] = Nil)
 
-sealed trait Validator[T <: JValue] {
-  def apply(v: JValue): ValidationNel[ValidationError, T]
-}
-
-sealed trait ValidatorProto[T <: JValue] {
-  def validator: Validator[T]
-}
-
-case class ObjectValidator(properties: Map[String, Validator[_ <: JValue]]) extends Validator[JObject] {
-  private def prefixErrors(propName: String, errors: NonEmptyList[ValidationError]): List[ValidationError] =
-    errors.map(e => e.copy(path = propName :: e.path)).toList
-
-  private def validateProps(obj: JObject): ValidationNel[ValidationError, JObject] =
-    properties.flatMap {
-      case (name, v) => v(obj \ name).fold(errors => prefixErrors(name, errors), _ => Nil)
-    }.toList.toNel.fold(obj.successNel[ValidationError])(_.failure[JObject])
-
-  def apply(v: JValue): ValidationNel[ValidationError, JObject] =
-    v.isInstanceOf[JObject]
-      .option(v.asInstanceOf[JObject])
-      .fold(ValidationError("Expected object, got " + v).failureNel[JObject])(validateProps)
-}
-
-class DummyValidator[T <: JValue](implicit ct: ClassTag[T]) extends Validator[T] {
-  def apply(v: JValue): ValidationNel[ValidationError, T] = v match {
-    case ct(value) => value.successNel[ValidationError]
-    case _ => ValidationError(s"Expected ${ct.runtimeClass.getName}, got " + v.getClass.getName).failureNel[T]
-  }
-}
-
 object Jsonz {
-  implicit val JObjectValidatorProto: ValidatorProto[JObject] = new ValidatorProto[JObject] { def validator = ObjectValidator(Map.empty) }
-  implicit val JIntValidatorProto: ValidatorProto[JInt] = new ValidatorProto[JInt] { def validator = new DummyValidator }
-  implicit val JBoolValidatorProto: ValidatorProto[JBool] = new ValidatorProto[JBool] { def validator = new DummyValidator }
-  implicit val JStringValidatorProto: ValidatorProto[JString] = new ValidatorProto[JString] { def validator = new DummyValidator }
-  implicit val JArrayValidatorProto: ValidatorProto[JArray] = new ValidatorProto[JArray] { def validator = new DummyValidator }
-  implicit val JDoubleValidatorProto: ValidatorProto[JDouble] = new ValidatorProto[JDouble] { def validator = new DummyValidator }
+  type JsonValidation[T] = ValidationNel[ValidationError, T]
+  type Validator[T <: JValue] = JValue => JsonValidation[T]
 
-  def isA[T <: JValue](implicit p: ValidatorProto[T]): Validator[T] = p.validator
+  implicit def jvalueSemigroup[T <: JValue] = Semigroup.lastSemigroup[T]
 
-  implicit class ObjectValidatorOps(validator: Validator[JObject]) {
-    def withProperties(props: (String, Validator[_ <: JValue])*): Validator[JObject] = withProperties(props.toMap)
-    def withProperties(props: Map[String, Validator[_ <: JValue]]): Validator[JObject] = ObjectValidator(props)
+  implicit def validatorMonoid[T <: JValue](implicit ct: ClassTag[T]) = new Monoid[Validator[T]] {
+    def zero: Validator[T] = _ match {
+      case ct(v) => v.asInstanceOf[T].successNel
+      case v => ValidationError(s"Expected instance of ${ct.runtimeClass.getName}, got ${v.getClass.getName}").failureNel
+    }
+    def append(a: Validator[T], b: => Validator[T]): Validator[T] = v => { a(v) +++ b(v) }
+  }
+
+  implicit class StringValidatorOps(v: Validator[JString]) {
+    def shouldMatch(r: Regex): Validator[JString] = v |+| {
+      case jv @ JString(s) if s.matches(r.toString) => jv.successNel[ValidationError]
+      case JString(s) => ValidationError(s"The string $s doesn't match the pattern ${r.toString}").failureNel[JString]
+    }
+  }
+
+  implicit class ObjectValidatorOps(v: Validator[JObject]) {
+    private def validatorForProp[T <: JValue](prop: String, validator: Validator[T]): Validator[JObject] = { jv =>
+      validator(jv \ prop).bimap(
+        _.map(err => err.copy(path = prop :: err.path)),
+        _ => jv.asInstanceOf[JObject]) // TODO: Remove ugly asInstanceOf
+    }
+
+    def mustHave[T](props: Map[String, Validator[_ <: JValue]]): Validator[JObject] =
+      props.foldLeft(v) { (v, prop) =>
+        v |+| validatorForProp(prop._1, prop._2)
+      }
+
+    def mustHave[T](props: (String, Validator[_ <: JValue])*): Validator[JObject] = mustHave[T](props.toMap)
+  }
+
+  def validated[T <: JValue: ClassTag] = Monoid[Validator[T]].zero
+
+  def optional[T <: JValue: ClassTag](v: Validator[T]): Validator[JValue] = { jv =>
+    jv match {
+      case JNothing | JNull => jv.successNel[ValidationError]
+      case _ => v(jv)
+    }
   }
 }
--- src/test/scala/astrac/jsonz/JsonzSpecs.scala
+++ src/test/scala/astrac/jsonz/JsonzSpecs.scala
@@ -16,30 +16,65 @@ class JsonzSpecs extends FlatSpec with Matchers {
   "The Jsonz DSL" should "validate a json object" in {
     val json = parse("""{ "foo": 1, "bar": { "baz": true, "qux": [ 1, 2, 3 ] }, "quz": "blag", "dub": 1.2 }""")
 
-    val simpleValidator = isA[JObject] withProperties (
-      "foo" -> isA[JInt],
-      "bar" -> (isA[JObject] withProperties (
-        "baz" -> isA[JBool],
-        "qux" -> isA[JArray]
+    val validator = validated[JObject] mustHave (
+      "foo" -> validated[JInt],
+      "bar" -> (validated[JObject] mustHave (
+        "baz" -> validated[JBool],
+        "qux" -> validated[JArray]
       )),
-      "quz" -> isA[JString],
-      "dub" -> isA[JDouble]
+      "quz" -> validated[JString],
+      "dub" -> validated[JDouble]
     )
 
-    simpleValidator(json) should equal(Success(json))
+    validator(json) should equal(Success(json))
   }
 
   it should "collect errors and keep track of where they happened" in {
     val json = parse("""{ "foo": 1, "bar": false, "baz": { "qux": false } }""")
 
-    val simpleValidator = isA[JObject] withProperties (
-      "foo" -> isA[JString],
-      "bar" -> isA[JString],
-      "baz" -> (isA[JObject] withProperties (
-        "qux" -> isA[JInt]
+    val validator = validated[JObject] mustHave (
+      "foo" -> validated[JString],
+      "bar" -> validated[JString],
+      "baz" -> (validated[JObject] mustHave (
+        "qux" -> validated[JInt]
       ))
     )
 
-    getPropertyErrors(simpleValidator(json)) should contain theSameElementsAs("foo" :: "bar" :: "baz/qux" :: Nil)
+    getPropertyErrors(validator(json)) should contain theSameElementsAs("foo" :: "bar" :: "baz/qux" :: Nil)
+  }
+
+  it should "accept missing/null properties in an object if they are optional" in {
+    val jsonMissing = parse("""{ "foo": 1, "bar": { "baz": true, "qux": [ 1, 2, 3 ] }, "quz": "blag", "dub": 1.2 }""")
+    val jsonNull = parse("""{ "foo": 1, "bar": { "baz": true, "qux": [ 1, 2, 3 ] }, "quz": "blag", "dub": 1.2, "puk": null }""")
+
+    val validator = validated[JObject] mustHave (
+      "foo" -> validated[JInt],
+      "bar" -> (validated[JObject] mustHave (
+        "baz" -> validated[JBool],
+        "qux" -> validated[JArray]
+      )),
+      "quz" -> validated[JString],
+      "dub" -> validated[JDouble],
+      "puk" -> optional(validated[JString])
+    )
+
+    validator(jsonMissing) should equal(Success(jsonMissing))
+    validator(jsonNull) should equal(Success(jsonNull))
+  }
+
+  it should "provide errors if an optional property is provided but not validated" in {
+    val json = parse("""{ "foo": 1, "bar": { "baz": true, "qux": [ 1, 2, 3 ] }, "quz": "blag", "dub": false }""")
+
+    val validator = validated[JObject] mustHave (
+      "foo" -> validated[JInt],
+      "bar" -> (validated[JObject] mustHave (
+        "baz" -> validated[JBool],
+        "qux" -> validated[JArray]
+      )),
+      "quz" -> validated[JString],
+      "dub" -> optional(validated[JDouble])
+    )
+
+    getPropertyErrors(validator(json)) should contain theSameElementsAs("dub" :: Nil)
   }
 }
